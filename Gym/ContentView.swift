245b321027196e38bba621561272106738ce1823
Shahinoor Shahin <shahin@braincraftapps.com>
2024-09-11 10:00:24 +0600

    Project initiated

---
+++ Gym/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gym
+//
+//  Created by Shahin Noor on 11/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gym/GymApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymApp.swift
+//  Gym
+//
+//  Created by Shahin Noor on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
